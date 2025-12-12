3f9bb25275cc533d66955b434cf2ceae919adec3
Shaswata Sarkar <shaswatasarkar05@gmail.com>
2025-12-12 21:54:38 +0530

    Frontend started

---
+++ client/src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Login from './pages/Login'
+import Home from './pages/Home'
+import EmailVerify from './pages/EmailVerify'
+import ResetPassword from './pages/ResetPassword'
+
+const App = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/verify-email' element={<EmailVerify />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App
