43d94883fedc4a372bda4b0a07d09f2d75f8551c
vagishvedansh <vagishvedansh@gmail.com>
2025-12-13 10:02:00 +0530

    vagish

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Routes, Route} from 'react-router-dom'
+import './App.css'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Home from './pages/Home'
 import EmailVerify from './pages/EmailVerify'
